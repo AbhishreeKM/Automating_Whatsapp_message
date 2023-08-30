e7b71f098b3e3226c526c9190c2db6e5d63da196
Abhishree K M <abhishreekm4738@gmail.com>
2023-08-30 12:58:26 +0530

    uploading sample project

---
+++ Birthday_bot.py
@@ -0,0 +1,14 @@
+# importing the module
+import pywhatkit
+
+try:
+    pywhatkit.sendwhatmsg("+918088217268", "Happy birthday Bhanu...hope all good for you, my lucky girl", 00, 00)
+    print("Successfully Sent!")
+except:
+   
+  # handling exception
+  # and printing error message
+  print("An Unexpected Error!")
+
+
+
